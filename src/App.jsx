ed8d7fd35427e3b951a7a798add8f94408426bf2
bruce <bmpandrada@gmail.com>
2025-09-05 09:28:04 +0800

    add limit show

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,67 @@
 import { useEffect } from "react";
 import { useState } from "react";
-const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-
+import CoinCard from "./components/CoinCard";
+const API_URL = import.meta.env.VITE_API_URL;
+// &order=market_cap_desc&per_page=10&page=1&sparkline=false`
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
- 
-    useEffect(()=> {
-      const dataCoins = async() => {
-        try {
-          const res = await fetch(API_URL);
-          if(!res.ok){
-            throw new Error('Data fetch failed');
-          }
-          const data = await res.json();
-          setCoins(data)
-          console.log(data);
-        } catch (error) {
-          setError(error.message)
-        }finally {
-          setLoading(false)
+  const [limit, setLimit] = useState(10);
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const res = await fetch(
+        `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+      );
+      try {
+        if (!res.ok) {
+          throw new Error("Data fetch request failed");
         }
+        const data = await res.json();
+        setCoins(data)
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
       }
-      dataCoins()
-    },[]);
 
+  
+    };
+        dataFetch();
+  }, [limit]);
 
-  return ( 
+  return (
     <div>
       <h1> 🚀 Crypto Dash</h1>
-      { loading && <p>Loading</p> }
-      { error && <div className="errpr">{error}</div> }
+      {loading && <p>Loading</p>}
+      {error && <div className="errpr">{error}</div>}
+      <div className="controls">
+        <label htmlFor="limit">Show:</label>
+        <select 
+          name="limit" 
+          id="limit" 
+          value={limit}
+          onChange={(e)=>setLimit(Number(e.target.value))}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
 
-      { !loading && !error && (
+        </select>
+      </div> 
+
+      {!loading && !error && (
         <main className="grid">
-          {coins.map((coin)=> (
-            <div className="coin-card" key={coin.id}>
-              <div className="coin-header">
-                <img className="coin-image" src={coin.image} alt={coin.name} />
-                <div>
-                  <h2>{coin.name}</h2>
-                  <p className="symbol">{coin.symbol.toUpperCase()}</p>
-                </div>
-              </div>
-              <p>Price: {coin.current_price.toLocaleString()}</p>
-              <p className={`${coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}`}>
-                {coin.price_change_percentage_24h} %
-              </p>
-              <p>
-                Market Cap: {coin.market_cap.toLocaleString()}
-              </p>
-            </div>
+          {coins.map((coin) => (
+            <CoinCard key={coin.key} coin={coin} />
           ))}
         </main>
-      ) }
+      )}
     </div>
-   );
-}
- 
+  );
+};
+
 export default App;
